a3134f01029aaf0dfec5bb5f3eb04552e1524444
HaolingPu <phling@umich.edu>
2024-04-22 23:05:22 -0400

    handle highlight

--- site/public/js/forum.js
+++ site/public/js/forum.js
@@ -2710,3 +2710,23 @@ function pinAnnouncement(thread_id, type, csrf_token) {
         });
     }
 }
+
+function newPostWhileUserOnline(){
+    const post_list = document.getElementById('posts_list');
+
+    const captureChange = new MutationObserver((mutationList , observer)=>{
+        for (let mutation of mutationList){
+            if (mutation.type === 'childList'){
+                mutation.addedNodes.forEach(node => {
+                    if (node.nodeType === Node.ELEMENT_NODE) {
+                        node.classList.add('new_post')
+                    }
+                });
+            }
+        }
+    });
+    captureChange.observe(post_list, { childList: true});
+}
+setTimeout(function(){
+    newPostWhileUserOnline();
+}, 4000);
